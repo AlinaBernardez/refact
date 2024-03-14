30c1bc5555f8c900986ce42c11311f35393b82f8
AlinaB <bernardezalina@gmail.com>
2024-03-14 11:22:23 +0100

    challenge

---
+++ src/componentes/Cabecera.jsx
@@ -0,0 +1,10 @@
+
+export const Cabecera = () => {
+    return (
+        <header className='cabecera'>
+            <a href="https://react.dev" target="_blank">
+                <img src='src\assets\react.svg' className="logo react" alt="React logo" />
+            </a>
+        </header>
+    )
+}
---
+++ src/componentes/Calculadora.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+export const Calucladora = () => {
+    const numbers = [0,1,2,3,4,5,6,7,8,9]
+    const operacion = ['+', '-', '*', '/']
+    const [input, setInput] = useState('');
+
+    const addToInput = value => {
+        setInput(input + value);
+    };
+    const clearInput = () => {
+        setInput('');
+    };
+    const calculateResult = () => {
+        try {
+            setInput(eval(input).toString());
+        } catch (error) {
+            setInput('Error');
+        }
+    };
+
+    return (
+        <div className='calculadora'>
+            <div className='input'>{input}</div>
+            <div className='numbers'>
+            {numbers.map((n, i) => (
+                <div onClick={() => addToInput(n)} key={i} id={n}>{n}</div>
+            ))}
+            </div>
+            <div className='operaciones'>
+            {operacion.map((op, i) => (
+                <div onClick={() => addToInput(op)} key={i} id={op}>{op}</div>
+            ))}
+            <div onClick={() => calculateResult()}>=</div>
+            <div onClick={() => clearInput()}>C</div>
+            </div>
+        </div>
+    )
+}
